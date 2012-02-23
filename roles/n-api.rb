46ceab3bb5b6f8671b0fc98129cace1658f16b68
oleg gordienko <ogordienko@mirantis.com>
2012-02-23 18:20:10 +0200

    n-api role

--- roles/n-api.rb
+++ roles/n-api.rb
@@ -1,5 +1,5 @@
 name "n-api"
 description "nova-api"
 run_list  "recipe[wheel::stack]"
-default_attributes "stack" => { "enabled_services" => [ "n-api"] }
+override_attributes "wheel::stack" => { "enabled_services" => "n-api" }
 
--- roles/n-sh.rb
+++ roles/n-sh.rb
@@ -1,4 +1,4 @@
 name "n-sh"
 description "nova-shedule"
 run_list  "recipe[wheel::stack]"
-default_attributes "stack" => { "enabled_services" => [ "n-sh"] }
+override_attributes "wheel::stack" => { "enabled_services" => "n-sh" }
